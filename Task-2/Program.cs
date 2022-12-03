afb4b17501f6c7174a8a243ad7f3eaf461ff177f
Maks <kpasowsky@gmail.com>
2022-12-03 13:54:14 +0300

    Task 2

---
+++ Task-2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+    Console.WriteLine($"max = {a}, min = {b}");
+if(a < b)
+    Console.WriteLine($"max = {b}, min = {a}");
