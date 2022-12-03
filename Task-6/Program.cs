0f1ea98bc8bf6caba7859270e721ed3a3b180f9c
Maks <kpasowsky@gmail.com>
2022-12-03 14:05:09 +0300

    Task 6

---
+++ Task-6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+    if ((x % 2) == 0)
+        Console.WriteLine($"{x} четное число");
+    else 
+         Console.WriteLine($"{x} нечетное число");
