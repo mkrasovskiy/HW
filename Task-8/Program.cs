89ddd87e5dea76501594967171f8bf0f76f4d141
Maks <kpasowsky@gmail.com>
2022-12-03 15:07:50 +0300

    Task 8

---
+++ Task-8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+ Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    if (i % 2 == 0)
+        Console.Write(i + ", ");
+         i++;
+}
