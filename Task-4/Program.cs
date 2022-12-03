e3f9c44653221645395d4052a870cf41410fce40
Maks <kpasowsky@gmail.com>
2022-12-03 13:50:15 +0300

    Task 4

---
+++ Task-4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(b < a)
+if(a > c)
+    Console.WriteLine($"max = {a}");
+if(a < b)
+if(b > c)
+    Console.WriteLine($"max = {b}");
+if(b < c)
+if(c > b)
+    Console.WriteLine($"max = {c}");
