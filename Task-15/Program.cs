ca6858899ec3504b94faae288a4bc5345cb3758b
Maks <kpasowsky@gmail.com>
2022-12-05 22:58:40 +0300

    Task 15

---
+++ Task-15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int dn = Convert.ToInt32(Console.ReadLine());
+if (dn == 6 || dn == 7)
+    Console.WriteLine("Выходной день");
+else if (dn < 1 || dn > 7)
+    Console.WriteLine("Это не день недели");
+else 
+    Console.WriteLine("Не выходной день");
+
