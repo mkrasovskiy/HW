ddfe26865d5cbbfb2da72c2a3a619327b9f0a371
Maks <kpasowsky@gmail.com>
2022-12-05 21:26:23 +0300

    Task 13

---
+++ Task-13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+while (n > 999)
+{
+    n = n / 10;
+}
+n = n % 10;
+Console.WriteLine(n);
