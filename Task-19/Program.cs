d89d6b415336c0dd9505716d5eaa10bd9738f3a3
Maks <kpasowsky@gmail.com>
2022-12-08 23:05:42 +0300

    Task 19

---
+++ Task-19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string n = Console.ReadLine();
+if (n[0] == n[4] || n[1] == n[3])
+    Console.WriteLine("Палиндром");
+else 
+    Console.WriteLine("Не палиндром");
+
+
+
+
+
+
