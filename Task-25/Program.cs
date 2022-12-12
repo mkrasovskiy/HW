9560630c1b788dc2fabc2ebd97b2a05f9f550351
Maks <kpasowsky@gmail.com>
2022-12-12 21:59:45 +0300

    Task-25

---
+++ Task-25/Program.cs
@@ -0,0 +1,10 @@
+﻿  Console.Write("Введите первое число: ");
+  int n = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  int m = Convert.ToInt32(Console.ReadLine());
+  int res = 1;
+  for (int i = 0; i < m; i++) 
+  {
+   res *= n;
+  }
+  Console.WriteLine($"{res}");
