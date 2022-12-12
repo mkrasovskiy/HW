01dee6ac5c28eec3796a15d131a988a672a10218
Maks <kpasowsky@gmail.com>
2022-12-13 00:40:29 +0300

    Task 29

---
+++ Task-29/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(0, n);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+ 
