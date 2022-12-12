7866cb1358883ba921bb7cc4015eb73624eae863
Maks <kpasowsky@gmail.com>
2022-12-12 22:16:08 +0300

    Task 27

---
+++ Task-27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0) 
+{
+    int a = n % 10;
+    n /= 10;
+    sum += a;
+}       
+Console.WriteLine(sum);
