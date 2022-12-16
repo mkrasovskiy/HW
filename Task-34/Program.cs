6fa557aba36872ca264db19455613e11f5af124f
Maks <kpasowsky@gmail.com>
2022-12-17 00:03:22 +0300

    Task 34

---
+++ Task-34/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+
+int[] InputArray(int[] array)
+{
+    for(int m = 0; m < array.Length; m++)
+        array[m] = new Random().Next(100,1000);
+
+    Console.WriteLine($"Массив [{string.Join(", ", array)}]");
+    return array;
+}
+
+void RealiseArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0)
+            count++;
+
+    Console.WriteLine($"Количество четных чисел: {count}");
+}
+
+Console.Write("Введите размер массива: ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+RealiseArray(InputArray(array));
