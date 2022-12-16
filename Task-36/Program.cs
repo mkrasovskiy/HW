b625788ba0d62486f4b9963390b915c442f86836
Maks <kpasowsky@gmail.com>
2022-12-17 00:28:57 +0300

    Task 36

---
+++ Task-36/Program.cs
@@ -0,0 +1,25 @@
+﻿int sum = 0;
+
+int[] InputArray(int[] array)
+{
+    for(int m = 0; m < array.Length; m++)
+        array[m] = new Random().Next(0,100);
+
+    Console.WriteLine($"Массив [{string.Join(", ", array)}]");
+    return array;
+}
+
+void SumElements(int[] array)
+{
+    for (int i = 1; i < array.Length; i+=2)
+        sum += array[i];
+
+    Console.WriteLine($"Сумма нечетных элементов массива: {sum}");
+}
+
+Console.Write("Введите размер массива: ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+SumElements(InputArray(array));
