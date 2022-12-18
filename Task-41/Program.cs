d9858994c4305e3a6eccc5e114329da5f5a1e0b9
Maks <kpasowsky@gmail.com>
2022-12-19 00:05:05 +0300

    Task 41

---
+++ Task-41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы (через пробел): ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+ 
+for (int i = 0; i < size.Length; i++)
+{
+    if (size[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
