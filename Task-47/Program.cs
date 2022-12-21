7c0912e8ca1d9e06e10fd221e7c358a37d42afb2
Maks <kpasowsky@gmail.com>
2022-12-21 22:41:55 +0300

    Task 47

---
+++ Task-47/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (100.0 - 0.0) + 0.0, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+
+InputMatrix(matrix);
