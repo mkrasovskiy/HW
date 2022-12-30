93721c9fad0d20e2de37e31c57552bef4bdb2447
Maks <kpasowsky@gmail.com>
2022-12-30 20:49:55 +0300

    Task 66

---
+++ Task-66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
